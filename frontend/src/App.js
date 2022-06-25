5c30439ae4cbaa56cfb4d0c4fc182e5f429d16a1
musamarabbani <usama.rabbani@invozone.com>
2022-06-25 15:46:36 +0500

    sockets event handling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,20 +4,41 @@ import { nanoid } from 'nanoid';
 import './App.css';
 
 const socket = io('http://localhost:5000');
-
-function App() {
+const userId = nanoid(5);
+const App = () => {
 	const [message, setMessage] = useState('');
 	const [chat, setChat] = useState([]);
 
+	useEffect(() => {
+		socket.on('chat', (payload) => {
+			setChat([...chat, payload]);
+		});
+	});
+
 	const sendChat = (e) => {
 		e.preventDefault();
-		socket.emit('chat', { message: message });
+		socket.emit('chat', { message: message, userId });
 		setMessage('');
 	};
 	return (
 		<div className='App'>
 			<header className='App-header'>
 				<h1>Chatty App</h1>
+				{chat &&
+					chat.map((payload, index) => (
+						<p key={index}>
+							<span
+								style={{
+									background: 'green',
+									color: '#fff',
+									marginRight: '10px',
+								}}
+							>
+								userId: {payload.userId}
+							</span>
+							{payload.message}
+						</p>
+					))}
 				<form onSubmit={sendChat}>
 					<input
 						type='text'
@@ -31,6 +52,6 @@ function App() {
 			</header>
 		</div>
 	);
-}
+};
 
 export default App;
