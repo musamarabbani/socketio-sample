5b29450cee281e5a25122237d3a0dac1e301c371
musamarabbani <usama.rabbani@invozone.com>
2022-06-25 15:02:40 +0500

    socket backend setup

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+
+function App() {
+	return (
+		<div className='App'>
+			<header className='App-header'>Chat App</header>
+		</div>
+	);
+}
+
+export default App;
