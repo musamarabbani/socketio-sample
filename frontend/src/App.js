f1a6caaff63bdd62d7871ad5184dc833f63574f1
musamarabbani <usama.rabbani@invozone.com>
2022-06-25 15:17:05 +0500

    socket integration

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,34 @@
 import React, { useState, useEffect } from 'react';
+import io from 'socket.io-client';
+import { nanoid } from 'nanoid';
 import './App.css';
 
+const socket = io('http://localhost:5000');
+
 function App() {
+	const [message, setMessage] = useState('');
+	const [chat, setChat] = useState([]);
+
+	const sendChat = (e) => {
+		e.preventDefault();
+		socket.emit('chat', { message: message });
+		setMessage('');
+	};
 	return (
 		<div className='App'>
-			<header className='App-header'>Chat App</header>
+			<header className='App-header'>
+				<h1>Chatty App</h1>
+				<form onSubmit={sendChat}>
+					<input
+						type='text'
+						name='chat'
+						placeholder='send text'
+						value={message}
+						onChange={(e) => setMessage(e.target.value)}
+					/>
+					<button type='submit'>Send</button>
+				</form>
+			</header>
 		</div>
 	);
 }
